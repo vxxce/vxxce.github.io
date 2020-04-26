d5519324aec3d63d436c433dcff9509e6938c5d2
Zach <info@zacharyolp.in>
2020-04-26 07:23:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const title = document.querySelector("h1.title")
 const main = document.querySelector("main")
-let titleX = 10
-let titleY = 10
+
+let titleX, titleY = 10
 let [r, g, b] = [200, 180, 170]
 let score = 0
 let titleSize = 2
@@ -9,7 +9,7 @@ let titleBorder = 0.5
 
 const randomizeEm = n => {
   if (score > 800) {
-    main.innerHTML = "YOU WIN"
+    main.textContent = "YOU WIN"
     main.style.textAlign = "center"
     main.style.fontSize = "18rem"
     main.style.fontWeight = 700
@@ -20,22 +20,15 @@ const randomizeEm = n => {
     titleY -= 5
     titleBorder += 0.1
     if (score > 1200) {
-      window.addEventListener(
-        "mousemove",
-        event => event.stopPropagation(),
-        true
-      )
+      window.addEventListener("mousemove", e => e.stopPropagation(), true)
       return
     }
   }
   titleSize += 0.015
-  title.style.fontSize = titleSize.toString().concat("rem")
+  title.style.fontSize = titleSize.toString() + "rem"
   titleBorder += 0.0015
-  title.style.borderBottom = `${titleBorder
-    .toString()
-    .concat("rem")} solid rgb(231, 222, 210)`
-  title.style.transform = `translate(${titleX.toString() +
-    "px"}, ${titleY.toString() + "px"})`
+  title.style.borderBottom = `${titleBorder.toString() + "rem"} solid rgb(231, 222, 210)`
+  title.style.transform = `translate(${titleX.toString() + "px"}, ${titleY.toString() + "px"})`
   title.style.color = `rgb(${r}, ${g}, ${b})`
   titleX += Math.round(Math.random())
     ? Math.round(Math.random() * 9 + n)
