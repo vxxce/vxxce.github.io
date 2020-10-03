e83d6f0b06252710074d88b353be64e90acd038d
Zachary Olpin <info@zacharyolp.in>
2020-10-02 23:47:39 -0600

    extract functions, add eventListener cleanup

--- index.js
+++ index.js
@@ -8,23 +8,32 @@ let score = 0
 let titleSize = 2
 let titleBorder = 0.5
 
+const clearListeners = () => {
+  document.removeEventListener("mousemove", mouse)
+  document.removeEventListener("click", click)
+  document.removeEventListener("scroll", scroll)
+}
+
+const displayWin = () => {
+  document.body.style.height = "100vh"
+  document.body.style.width = "100%"
+  document.body.style.overflow = "hidden"
+  main.textContent = "YOU WIN"
+  main.style.textAlign = "center"
+  main.style.fontSize = "18rem"
+  main.style.fontWeight = 700
+  main.style.fontFamily = "Helvetica, Open Sans, Arial, sans-serif"
+  main.style.color = "#ffff00"
+  titleSize += 0.2
+  titleX -= 5
+  titleY -= 5
+  titleBorder += 0.1
+  title.style.zIndex = 0
+  main.style.position = "relative"
+  main.style.zIndex = 1
+}
+
 const randomizeEm = n => {
-  if (score > 800) {
-    main.textContent = "YOU WIN"
-    main.style.textAlign = "center"
-    main.style.fontSize = "18rem"
-    main.style.fontWeight = 700
-    main.style.fontFamily = "Helvetica, Open Sans, Arial, sans-serif"
-    main.style.color = "#ffff00"
-    titleSize += 0.2
-    titleX -= 5
-    titleY -= 5
-    titleBorder += 0.1
-    if (score > 1200) {
-      window.addEventListener("mousemove", e => e.stopPropagation(), true)
-      return
-    }
-  }
   titleSize += 0.015
   title.style.fontSize = titleSize.toString() + "rem"
   titleBorder += 0.0015
@@ -55,8 +64,18 @@ const randomizeEm = n => {
     ? Math.round(Math.random() * 5)
     : -Math.round(Math.random() * 5)
   score++
+  if (score > 800) {
+    displayWin()
+    if (score > 1400) {
+      clearListeners()
+    }
+  }
 }
 
-document.addEventListener("mousemove", () => randomizeEm(1))
-document.addEventListener("click", () => randomizeEm(20))
-document.addEventListener("scroll", () => randomizeEm(20))
+const mouse = () => randomizeEm(1)
+const click = () => randomizeEm(20)
+const scroll = () => randomizeEm(20)
+
+document.addEventListener("mousemove", mouse)
+document.addEventListener("click", click)
+document.addEventListener("scroll", scroll)
